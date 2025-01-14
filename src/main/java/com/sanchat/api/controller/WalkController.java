6e37b80c69a5b6495f2a3c5fe71903ea9a5eae4c
Seonghun An <53287605+shAn-kor@users.noreply.github.com>
2025-01-14 21:57:28 +0900

    Feat: Add Walk Insert (#23)
    - 산책 정보 DB 에 저장
    
    Co-authored-by: hyejux <hyejux@gmail.com>
    Co-authored-by: JU.U <129069292+hyejux@users.noreply.github.com>

--- src/main/java/com/sanchat/api/controller/WalkController.java
+++ src/main/java/com/sanchat/api/controller/WalkController.java
@@ -3,31 +3,25 @@ package com.sanchat.api.controller;
 import com.sanchat.api.dto.WalkDTO;
 import com.sanchat.api.service.WalkService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/walk")
+@CrossOrigin(origins = "*")
 public class WalkController {
+
     @Autowired
-    private WalkService walkService;
+    WalkService walkService;
 
-    @GetMapping("/insert")
-    public void insertWalk(WalkDTO walkDTO) {
-        walkService.insertWalk(walkDTO);
+    @PostMapping("/walkData")
+    public ResponseEntity<String> walkData(@RequestBody WalkDTO data){
+
+        System.out.println(data);
+        walkService.walkRegist(data);
+        return ResponseEntity.ok("Data received successfully.");
     }
 
-    @GetMapping("/delete")
-    public void deleteWalk(Long walkNo) {
-        walkService.deleteWalk(walkNo);
-    }
 
-    @GetMapping("/select")
-    public List<WalkDTO> getAllWalk() {
-        return walkService.getAllWalk();
-    }
+
 }
--- src/main/java/com/sanchat/api/mapper/WalkMapper.java
+++ src/main/java/com/sanchat/api/mapper/WalkMapper.java
@@ -1,19 +1,11 @@
 package com.sanchat.api.mapper;
 
 import com.sanchat.api.dto.WalkDTO;
-import org.apache.ibatis.annotations.*;
-
-import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface WalkMapper {
-    @Insert("insert into walk(walk_time, walk_distance, walk_date) values(#{walkDTO.walkTime}, #{walkDTO.walkDistance}, #{walkDTO.walkDate})")
-    @Options(useGeneratedKeys = true, keyProperty = "walkNo")
-    void insertWalk(@Param("walkDTO") WalkDTO walkDTO);
 
-    @Delete("delete from walk where walk_no=#{walkNo}")
-    void deleteWalk(Long walkNo);
+    void walkRegist(WalkDTO dto);
 
-    @Select("select * from walk order by walk_no desc")
-    List<WalkDTO> getAllWalk();
 }
