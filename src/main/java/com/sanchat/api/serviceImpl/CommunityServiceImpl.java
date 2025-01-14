e422b6522eb02481af10b5f5ef65fdcd461056ab
Seonghun An <53287605+shAn-kor@users.noreply.github.com>
2025-01-14 21:54:18 +0900

    Feat: Add Posting (#22)
    - Cloudinary 라이브러리 추가
    - Cloudinary 설정 환경변수 .env 추가
    - Community 탭 관련 DTO 추가
    - TABLE.txt 커뮤니티 추가
    - Cloudinary 사진 업로드 완료
    - CommunityWrite 새글 DB 저장
    
    Co-authored-by: hjKim1597 <hojinkim971597@gmail.com>

--- src/main/java/com/sanchat/api/serviceImpl/CommunityServiceImpl.java
+++ src/main/java/com/sanchat/api/serviceImpl/CommunityServiceImpl.java
@@ -2,9 +2,14 @@ package com.sanchat.api.serviceImpl;
 
 import com.sanchat.api.dto.CommunityDTO;
 import com.sanchat.api.mapper.CommunityMapper;
+import com.sanchat.api.service.CloudinaryService;
 import com.sanchat.api.service.CommunityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
 
 @Service
 public class CommunityServiceImpl implements CommunityService {
@@ -12,8 +17,22 @@ public class CommunityServiceImpl implements CommunityService {
     @Autowired
     private CommunityMapper communityMapper;
 
+    @Autowired
+    private CloudinaryService cloudinaryService;
+
     @Override
-    public CommunityDTO testCommunity() {
-        return communityMapper.selectTestCommunity();
+    public CommunityDTO newPost(String communityContent, MultipartFile file) throws IOException {
+
+        String uploadedUrl = cloudinaryService.upload(file);
+
+        CommunityDTO communityDTO = CommunityDTO.builder()
+                .communityContent(communityContent)
+                .filePath(uploadedUrl)
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        communityMapper.newPost(communityDTO);
+
+        return communityDTO;
     }
 }
